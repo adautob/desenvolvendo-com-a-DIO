e55ad31ae8197f1da9eec49a3fb7c5c47624eb5f
Adauto Bueno <adauto.bueno@gmail.com>
2021-12-12 17:03:53 -0300

    Desafio Iniciante Java - Cognizant Developer

---
+++ Desafio-Java-Iniciante/Desafio1.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Desafio1 {
+    public static void main(String[] args) throws IOException {
+    	Scanner leitor = new Scanner(System.in);
+    	int N = leitor.nextInt();
+    	int A = leitor.nextInt() + leitor.nextInt();
+        
+    	if ( A > N )
+    		System.out.println("Deixa para amanha!");
+    	else
+    		System.out.println("Farei hoje!");
+    }
+	
+}
---
+++ Desafio-Java-Iniciante/Desafio3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class Desafio3 {
+  public static void main(String[] args){
+    
+    Scanner leitor = new Scanner(System.in);
+    
+    
+    int a = leitor.nextInt();
+    int b = leitor.nextInt();
+    
+  //digite o seu código
+    System.out.println("X = "+(a+b));
+    
+  }
+}
