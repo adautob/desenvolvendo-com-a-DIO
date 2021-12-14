756bf2ae576565f292d97a89fb4a3ccd27ac018e
Adauto Bueno <adauto.bueno@gmail.com>
2021-12-14 00:11:53 -0300

    Desafio1 acrescentado texto pedindo a entrada do usuário

--- Desafio-Java-Iniciante/Desafio1.java
+++ Desafio-Java-Iniciante/Desafio1.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 public class Desafio1 {
     public static void main(String[] args) throws IOException {
     	Scanner leitor = new Scanner(System.in);
+    	System.out.println("Digite quantos minutos faltam: ");
     	int N = leitor.nextInt();
-    	int A = leitor.nextInt() + leitor.nextInt();
-        
+    	System.out.println("Tempo fabricar presente 1: ");
+    	int A = leitor.nextInt();
+    	System.out.println("Tempo fabricar presente 2: ");
+    	A += leitor.nextInt();
     	if ( A > N )
     		System.out.println("Deixa para amanha!");
     	else
